340f40d8fc0427e71dea8c2daf4372857a888067
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-05-15 14:35:24 -0700

    Completed 33_sum_range

--- 33_sum_range/sum_range.py
+++ 33_sum_range/sum_range.py
@@ -1,3 +1,6 @@
+from ast import Num
+
+
 def sum_range(nums, start=0, end=None):
     """Return sum of numbers from start...end.
 
@@ -23,3 +26,8 @@ def sum_range(nums, start=0, end=None):
         >>> sum_range(nums, 1, 99)
         9
     """
+
+    if end is None:
+        end = len(nums)
+
+    return sum(nums[start:end + 1])
