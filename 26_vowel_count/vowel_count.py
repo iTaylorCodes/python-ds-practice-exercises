1cc45055cbdc2f44951f16610b00b9cd5df8a3dc
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-05-15 13:39:57 -0700

    Completed 26_vowel_count

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,13 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+
+    vowels = set('aeiou')
+
+    vowel_counter = {}
+
+    for char in phrase.lower():
+        if char in vowels:
+            vowel_counter[char] = vowel_counter.get(char, 0) + 1
+
+    return vowel_counter
